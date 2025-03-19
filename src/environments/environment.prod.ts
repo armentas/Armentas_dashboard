cf49890cdfda2709e0b863c3c917c926fa0a2ce9
YosuaniD2A <yosuani@d2america.com>
2025-03-19 11:35:34 -0700

    Ajuste en la ruta de railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://armentasshopbk-production.up.railway.app/api/dashboard'
-  apiUrl: 'https://armentasbackend-production.up.railway.app/api/dashboard' // Armentas Service
+  // apiUrl: 'https://armentasbackend-production.up.railway.app/api/dashboard'
+  apiUrl: 'https://armentasshopbk-production.up.railway.app/api/dashboard' // Armentas Service
 };
